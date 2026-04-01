8127eba0a91e98c9c12145f558d58a0a146b0338
sophia <sopiaslucas@gmail.com>
2026-04-01 14:33:25 -0300

    Update Footer component styles

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,10 +1,12 @@
 export default function Footer(){
     return(
         <header style={{
-      backgroundColor: '#4682B4',
+      backgroundColor: '#4f7ea8',
       color: 'white',
-      padding: '10px',
-      textAlign: 'center'}}>
+      padding: '15px',
+      borderRadius: '6px',
+      textAlign: 'center',
+      marginTop: '20px'}}>
       <p>Projeto React - Infonet</p>
     </header>
     )
